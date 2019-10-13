065811211e0be6b4da04091f2e820dcb8a2deddb
jaerodyne <jsomera@gmail.com>
2019-10-13 18:03:19 -0500

    Add react bootstrap and display randomized quote

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Quote from './Quote';
 
 function App() {
--- src/Quote.js
+++ src/Quote.js
@@ -1,31 +1,92 @@
 import React, { Component } from 'react';
 import BojackQuotes from './bojack-quotes.json';
+import { Button, Card } from 'react-bootstrap';
 
 class Quote extends Component {
-  state = { quotes: [] }
+  state = { 
+    quotes: [],
+    displayedQuote: null
+  }
 
   componentDidMount() {
-    let quotes = BojackQuotes.quotes.map((quote, index) => {
+    let quotes = BojackQuotes.quotes.map((quote) => {
       return (
-        <li key={index}>
-          {quote.quote}
-          <span id="author">
+        <h1
+          style={{
+            marginTop: '5px'
+          }}
+        >
+          <p 
+            style={{
+              fontSize: '32px'
+            }}
+          >
+            {quote.quote}
+          </p>
+          <p 
+            id="author"
+            style={{
+              fontSize: '24px'
+            }}
+          >
             -- {quote.author}
-          </span>
-        </li>
+          </p>
+        </h1>
       )
     })
-    this.setState({quotes});
+    const quote = quotes[this.randomIndex(quotes)]
+    this.setState({
+      quotes,
+      displayedQuote: quote
+    });
+  }
+
+  getNewQuote() {
+    const quote = this.state.quotes[this.randomIndex(this.state.quotes)]
+    this.setState({
+      displayedQuote: quote
+    })
+  }
+
+  randomIndex(quotes) {
+    if (this.state.quotes) {
+      return Math.floor(Math.random() * this.state.quotes.length)
+    } else {
+      return Math.floor(Math.random() * quotes.length)
+    }
   }
 
   render() {
     return(
-      <div id="quote-box">
-        <h1 id="text">
-          {this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)]}
-        </h1>
-        <button id="new-quote">New Quote</button>
-        <a href="tweet-quote" id="tweet-quote">Tweeter</a>
+      <div className="container" id="quote-box">
+        <Card
+          style={{
+            border: 'none',
+            fontFamily: 'Lora, serif',
+            margin: '15px',
+            opacity: '0.8',
+            padding: '15px',
+            width: '600px'
+          }}
+        >
+          <div id="text">
+            {this.state.displayedQuote}
+          </div>
+          <div
+            style={{
+              fontFamily: 'Poppins, sans-serif',
+              margin: '15px'
+            }}
+          >
+            <Button onClick={() => this.getNewQuote()} id="new-quote">New Quote</Button>
+            <a
+              className="twitter-share-button"
+              datasize="large"
+              href={"https://twitter.com/intent/tweet?text="}>
+              Tweet
+            </a>
+          </div>
+        </Card>
       </div>
     )
   }
